2f712fe953ed6402065308130bc2341f7d6e7920
MUNHYUNBOO <ansgusqnwl@naver.com>
2025-05-07 01:52:01 +0900

    Swagger 문서화 기본 구성

--- term-search-api/src/main.ts
+++ term-search-api/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  const config = new DocumentBuilder()
+    .setTitle('산업용어 검색 API')
+    .setDescription('Swagger로 문서화된 산업용어 API입니다.')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);  // 여기가 Swagger 문서 경로
+  
+await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
