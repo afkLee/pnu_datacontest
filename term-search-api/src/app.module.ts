3499e2b6067dc913512cfc2a4c241f1de6215875
MUNHYUNBOO <ansgusqnwl@naver.com>
2025-05-07 14:03:56 +0900

    DB Connect

--- term-search-api/src/app.module.ts
+++ term-search-api/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),  // .env 사용을 위한 설정
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true, // 개발 시에만 true
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
